de962bfcfb8cd9125f2681359d83bd5822afdd04
一瞬流光* <karl.meng@qq.com>
2025-09-03 14:19:29 +0800

    feat: 添加自定义代码片段

--- lua/options.lua
+++ lua/options.lua
@@ -13,18 +13,20 @@ vim.g.neovide_scale_factor = 0.8
 vim.g.neovide_cursor_animation_length = 0
 vim.g.neovide_hide_mouse_when_typing = true
 
+vim.g.vscode_snippets_path = "~/.config/nvim/.vsnip"
+
 -- 使用PowerShell
 local powershell_options = {
   shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-  shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellcmdflag = "-NoLogo -NoProfile -ExearcutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+  shellredir = "-RedirectStandardOutput %-NoNewWindow -Wait",
   shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
   shellquote = "",
   shellxquote = "",
 }
 
 if vim.loop.os_uname().sysname == "Windows_NT" then
-  vim.cmd('language zh_CN.utf8')
+  vim.cmd "language zh_CN.utf8"
   for option, value in pairs(powershell_options) do
     vim.opt[option] = value
   end
